924a0630a026548381819e9bd3764f11ab126bd2
filippogheri <filippogheri@hotmail.com>
2014-07-25 11:00:02 +0200

    Change CountryAggregation to function

--- CountryAggregation.R
+++ CountryAggregation.R
@@ -6,13 +6,17 @@
 ## Notes:
 ###########################################################################
 
-country.df <- 
-  Aggregation(data = preAgg.df, aggVar = con.df[,"STS_ID"], 
-              thresholdProp = con.df[,"THRESHOLD_PROP"], 
-              weightVar = con.df[,"STS_ID_WEIGHT"], 
-              relationDF = na.omit(FAOcountryProfile[, c("FAOST_CODE", "M49_FAOST_CODE")]), 
-              aggMethod = con.df[,"AGGREGATION"], 
-              applyRules = FALSE,
-              keepUnspecified = FALSE)
-colnames(country.df)[grep("M49_FAOST_CODE", colnames(country.df))] = "FAOST_CODE"
-country.df[, "Area"] <- "Territory"
+CountryAggregation <- function(dataset = preAgg.df)}{
+  country.df <- 
+    Aggregation(data = dataset, aggVar = con.df[,"STS_ID"], 
+                thresholdProp = con.df[,"THRESHOLD_PROP"], 
+                weightVar = con.df[,"STS_ID_WEIGHT"], 
+                relationDF = na.omit(FAOcountryProfile[, c("FAOST_CODE", "M49_FAOST_CODE")]), 
+                aggMethod = con.df[,"AGGREGATION"], 
+                applyRules = FALSE,
+                keepUnspecified = FALSE)
+  colnames(country.df)[grep("M49_FAOST_CODE", colnames(country.df))] = "FAOST_CODE"
+  country.df[, "Area"] <- "Territory"
+  country.df
+}
+
